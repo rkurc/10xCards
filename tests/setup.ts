26603698e63355ee307d55994ce9b922358d841d
rkurc <roman.kurc@wp.pl>
2025-05-05 23:45:30 +0200

    remove old playwrite tests

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,16 +1,16 @@
-import { afterAll, afterEach, beforeAll } from 'vitest';
-import { cleanup } from '@testing-library/react';
-import * as matchers from '@testing-library/jest-dom/matchers';
-import { expect } from 'vitest';
-import { server } from './mocks/server';
+import { afterAll, afterEach, beforeAll } from "vitest";
+import { cleanup } from "@testing-library/react";
+import * as matchers from "@testing-library/jest-dom/matchers";
+import { expect } from "vitest";
+import { server } from "./mocks/server";
 
 // Extend vitest's expect method with methods from react-testing-library
 expect.extend(matchers);
 
 // Mock matchMedia
-Object.defineProperty(window, 'matchMedia', {
+Object.defineProperty(window, "matchMedia", {
   writable: true,
-  value: vi.fn().mockImplementation(query => ({
+  value: vi.fn().mockImplementation((query) => ({
     matches: false,
     media: query,
     onchange: null,
@@ -19,11 +19,11 @@ Object.defineProperty(window, 'matchMedia', {
     addEventListener: vi.fn(),
     removeEventListener: vi.fn(),
     dispatchEvent: vi.fn(),
-  }))
+  })),
 });
 
 // Setup mock service worker
-beforeAll(() => server.listen({ onUnhandledRequest: 'error' }));
+beforeAll(() => server.listen({ onUnhandledRequest: "error" }));
 afterEach(() => {
   cleanup();
   server.resetHandlers();
