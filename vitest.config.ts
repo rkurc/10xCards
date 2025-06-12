3043ad0f0e3c1ce57d8f7a9b18c140f252bddec8
rkurc <roman.kurc@wp.pl>
2025-06-13 00:48:22 +0200

    Lint

--- vitest.config.ts
+++ vitest.config.ts
@@ -31,6 +31,8 @@ export default defineConfig({
   },
   define: {
     "import.meta.env.PUBLIC_SUPABASE_URL": JSON.stringify("http://127.0.0.1:54321"),
-    "import.meta.env.PUBLIC_SUPABASE_ANON_KEY": JSON.stringify("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0"),
+    "import.meta.env.PUBLIC_SUPABASE_ANON_KEY": JSON.stringify(
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0"
+    ),
   },
 });
